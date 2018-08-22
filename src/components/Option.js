5839fd6dada762cccc6fc22fa185c4e04af1cb12
Loïc Gasser <loicgasser4@gmail.com>
2018-08-22 19:57:56 -0400

    Implicit return

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
-const Option = (props) => {
-    return (
-        <li>
-            {props.option}
-            <button
-                onClick={(e) => {
-                    props.handleDeleteOption(props.option)
-                }}
-            >remove</button>
-        </li>
-    )
-}
+const Option = (props) => (
+    <li>
+        {props.option}
+        <button
+            onClick={(e) => {
+                props.handleDeleteOption(props.option)
+            }}
+        >remove</button>
+    </li>
+)
 
 export default Option
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import Option from './Option'
 
-const Options = (props) => {
-    return (
-        <div>
-            <button onClick={props.handleDeleteOptions}>Remove All</button>
-            <ol>
-                {
-                    props.options.map((opt) => (
-                        <Option
-                            key={opt}
-                            option={opt}
-                            handleDeleteOption={props.handleDeleteOption}
-                        />
-                    ))
-                }
-            </ol>
-        </div>
-    )
-}
+const Options = (props) => (
+    <div>
+        <button onClick={props.handleDeleteOptions}>Remove All</button>
+        <ol>
+            {
+                props.options.map((opt) => (
+                    <Option
+                        key={opt}
+                        option={opt}
+                        handleDeleteOption={props.handleDeleteOption}
+                    />
+                ))
+            }
+        </ol>
+    </div>
+)
 
 export default Options
